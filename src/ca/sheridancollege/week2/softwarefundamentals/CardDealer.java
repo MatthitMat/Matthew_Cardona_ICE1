aeda93af0dfecbe3e1fab3e470051e74657099e0
MatthitMat <82432113+MatthitMat@users.noreply.github.com>
2022-02-01 12:48:25 -0500

    Update CardDealer.java
    I added the "card.getSuit" by editing here on github

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -19,5 +19,7 @@ public class CardDealer
       Card card = new Card("Spades", 4); //Added by Matthew Cardona
       Card nineClubs = new Card("Clbs", 9);
       nineClubs.setSuit("diamonds");
+      
+      card.getSuit();
    }
 }
